7df48132f79e004aea4a392e6ac617f5ad6fd710
1pkr1 <p.krych12@gmail.com>
2025-06-30 18:37:51 +0200

    Create basic method for User

--- src/main/java/com/patrykkrych/bank_app/rest/UserRestController.java
+++ src/main/java/com/patrykkrych/bank_app/rest/UserRestController.java
@@ -4,9 +4,7 @@ package com.patrykkrych.bank_app.rest;
 import com.patrykkrych.bank_app.entity.User;
 import com.patrykkrych.bank_app.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,5 +23,55 @@ public class UserRestController {
     public List<User> findAll() {
         return userService.findAll();
     }
+
+    @GetMapping("/users/{userId}")
+    public User getUser(@PathVariable int userId) {
+
+        User theUser = userService.findById(userId);
+
+        if (theUser == null) {
+            throw new RuntimeException("Employee id not found - " + userId);
+        }
+
+        return theUser;
+    }
+
+    @PostMapping("/users")
+    public User addUser(@RequestBody User theUser) {
+
+        theUser.setId(0);
+        User dbUser = userService.save(theUser);
+
+        return dbUser;
+    }
+
+    @PutMapping("/users")
+    public User updateUser(@RequestBody User theUser) {
+
+        User dbUser = userService.save(theUser);
+
+        return dbUser;
+    }
+
+    @DeleteMapping("/users/{userId}")
+    public String deleteUser(@PathVariable int userId) {
+
+        User tempUser = userService.findById(userId);
+
+        if (tempUser == null) {
+            throw new RuntimeException("User id not found - " + userId);
+        }
+
+        userService.deleteById(userId);
+
+        return "Deleted user with id - " + userId;
+    }
+
+
+
+
+
+
+
 
 }
